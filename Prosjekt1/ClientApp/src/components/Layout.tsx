e859f7881d6d765cc527334e386626bc4b8b3bfa
andordavoti <andor.davoti@gmail.com>
2021-09-27 16:15:10 +0200

    sticky footer

--- Prosjekt1/ClientApp/src/components/Layout.tsx
+++ Prosjekt1/ClientApp/src/components/Layout.tsx
@@ -1,5 +1,5 @@
 ﻿import { Container } from "@material-ui/core";
-import { FC } from "react";
+import { FC, useRef } from "react";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
 
@@ -8,12 +8,28 @@ interface Props {
 }
 
 const Layout: FC<Props> = ({ children }) => {
+  const navRef = useRef<null | HTMLDivElement>(null);
+  const footerRef = useRef<null | HTMLDivElement>(null);
   return (
-    <>
-      <Navbar />
-      <Container>{children}</Container>
-      <Footer />
-    </>
+    <div style={{ minHeight: "100vh" }}>
+      <div ref={navRef}>
+        <Navbar />
+      </div>
+
+      <Container
+        style={{
+          minHeight: `calc(100vh - ${navRef.current?.clientHeight || 0}px - ${
+            footerRef.current?.clientHeight || 0
+          }px)`,
+        }}
+      >
+        {children}
+      </Container>
+
+      <div ref={footerRef}>
+        <Footer />
+      </div>
+    </div>
   );
 };
 
